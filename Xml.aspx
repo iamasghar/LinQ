034fe326deec47f456d1bfb14136ffc4e781c363
Asghar Ali <34656340+iamasghar@users.noreply.github.com>
2020-01-02 00:27:49 +0500

    Add project files.

---
+++ Xml.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Xml.aspx.cs" Inherits="LinQ.Xml" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 33px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="width: 1380px; height: 278px">
+            <asp:GridView ID="GridView1" runat="server" Height="280" Width="1520">
+            </asp:GridView>
+        </div>
+        <hr />
+        <div>
+
+            <table style="width:100%;">
+                <tr>
+                    <td>Id</td>
+                    <td>
+                        <asp:TextBox ID="id" runat="server" Width="584px"></asp:TextBox>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>Name</td>
+                    <td>
+                        <asp:TextBox ID="name" runat="server" Width="581px"></asp:TextBox>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>Cell</td>
+                    <td>
+                        <asp:TextBox ID="cell" runat="server" Width="583px"></asp:TextBox>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>Address</td>
+                    <td>
+                        <asp:TextBox ID="address" runat="server" Width="581px"></asp:TextBox>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style1"></td>
+                    <td class="auto-style1">
+                        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Insert" />
+                        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Update" />
+                        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Delete" />
+                        <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Search" />
+                        <asp:Button ID="Button5" runat="server" OnClick="Button5_Click" Text="Load From DT" Width="109px" />
+                        <asp:Button ID="Button6" runat="server" OnClick="Button6_Click" Text="Load From Xml" Width="119px" />
+                        <asp:Button ID="Button7" runat="server" OnClick="Button7_Click" Text="DT -&gt; XML" Width="92px" />
+                    </td>
+                    <td class="auto-style1"></td>
+                </tr>
+            </table>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Xml.aspx.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace LinQ
+{
+    public partial class Xml : System.Web.UI.Page
+    {
+        String Filename = "D:/6th Sem/VP/LinQ/linq_Xml.xml";
+        DataTable dt;
+        XDocument xmldoc = XDocument.Load("D:/6th Sem/VP/LinQ/linq_Xml.xml");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void BindGrid()
+        {
+            //add xml document
+            var bind = xmldoc.Descendants("Employee").Select(p => new
+            {
+                emp_id = p.Element("emp_id").Value,
+                Name = p.Element("name").Value,
+                Cell = p.Element("cell").Value,
+                Address = p.Element("address").Value
+            }).OrderBy(p => p.emp_id);
+            GridView1.DataSource = bind;
+            GridView1.DataBind();
+        }
+        public SqlConnection DbConnection()
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-UL4JOR7;Initial Catalog=OfcDB;Integrated Security=True");
+            return conn;
+        }
+        public DataTable search(String query)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                SqlConnection conn = DbConnection();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                conn.Open();
+                da.Fill(dt);
+            }
+            catch (SqlException ex)
+            {
+                String exc = ex.Message;
+            }
+            return dt;
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            XElement emp = new XElement("Employee",
+                    new XElement("emp_id", id.Text),
+                    new XElement("name", name.Text),
+                    new XElement("cell", cell.Text),
+                    new XElement("address", address.Text));
+            xmldoc.Root.Add(emp);
+            xmldoc.Save(Filename);
+            BindGrid();
+            Reset();
+        }
+
+        private void Reset()
+        {
+            id.Text = "";
+            name.Text = "";
+            cell.Text = "";
+            address.Text = "";
+            id.Focus();
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            XElement emp = xmldoc.Descendants("Employee").FirstOrDefault(p => p.Element("emp_id").Value == id.Text);
+            if (emp != null)
+            {
+                emp.Element("name").Value = name.Text;
+                emp.Element("cell").Value = cell.Text;
+                emp.Element("address").Value = address.Text;
+                xmldoc.Save(Filename);
+                BindGrid();
+                Reset();
+
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            XElement emp = xmldoc.Descendants("Employee").FirstOrDefault(p => p.Element("emp_id").Value == id.Text);
+            if (emp != null)
+            {
+                emp.Remove();
+                xmldoc.Save(Filename);
+                BindGrid();
+                Reset();
+            }
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            XElement emp = xmldoc.Descendants("Employee").FirstOrDefault(p => p.Element("emp_id").Value == id.Text);
+            if (emp != null)
+            {
+                name.Text = emp.Element("name").Value;
+                cell.Text = emp.Element("cell").Value;
+                address.Text = emp.Element("address").Value;
+
+            }
+        }
+
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            string qry = "select emp_id,name,cell,address from employee";
+            dt = search(qry);
+            XDocument xmlDoc = new XDocument(
+                                  new XElement("Employees",
+                                  from f in dt.AsEnumerable()
+                                  select new XElement("Employee",
+                                      new XElement("emp_id", f.Field<string>("emp_id")),
+                                      new XElement("name", f.Field<string>("Name")),
+                                      new XElement("cell", f.Field<string>("cell")),
+                                      new XElement("address", f.Field<string>("address")))));
+
+            xmlDoc.Save(Filename);
+
+            //for (int i = 0; i < dt.Rows.Count; i++)
+            //{
+            //    XElement emp = new XElement(new XElement("emp_id",dt.Rows['emp_id'].ToString()));
+            //}
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            string qry = "select emp_id,name,cell,address from employee";
+            dt = search(qry);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            BindGrid();
+        }
+    }
+}
